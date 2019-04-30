0da9309658d88928d10e978c5f37929e465e76ca
cyz <caiyangzhi123@163.com>
2019-04-30 17:56:03 +0800

    提交

--- src/components/my/article/classify/Classify.vue
+++ src/components/my/article/classify/Classify.vue
@@ -83,7 +83,11 @@ export default {
   },
   mounted () {
     let v = this
-    $('.classify_content').on('mousewheel DOMMouseScroll', mouseScroll)
+    $('.classify_content').on('mousewheel DOMMouseScroll', mouseScroll
+    
+    
+    
+    )
     $('.classify_content').scroll(function () {
       let scrollHeight = $(this).eq(0).prop('scrollHeight')
       let divHeight = $('.classify_content').height()
