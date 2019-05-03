9efe4ad9e5686b260386062d72b9a8159cdea196
NoNameHero <caiyangzhi123@163.com>
2019-05-03 18:25:34 +0800

    基本变量

--- src/base_variable.js
+++ src/base_variable.js
@@ -1,6 +1,6 @@
 
-const baseUrl = 'http://localhost:8081/O.OBusinessPlan'
-// const baseUrl = 'http://39.97.99.250:8080/O.OBusinessPlan'
+// const baseUrl = 'http://localhost:8081/O.OBusinessPlan'
+const baseUrl = 'http://39.97.99.250:8080/O.OBusinessPlan'
 const adminUrl = baseUrl + '/api/admin'
 const roleUrl = baseUrl + '/api/authority/roles'
 const resouUrl = baseUrl + '/api/authority/resource'
