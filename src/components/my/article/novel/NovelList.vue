c94d4c3b2b5801b05821381580e75520d250a7cc
NoNameHero <caiyangzhi123@163.com>
2020-01-14 16:55:44 +0800

    提交

--- src/components/my/article/novel/NovelList.vue
+++ src/components/my/article/novel/NovelList.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="divContiner" style="background: rgb(236, 245, 255);margin-top: 15px;position: relative; height: 1000px">
+  <div class="divContiner" style="margin-top: 15px;position: relative; height: 1000px">
     <div>
       <div class="novelHead">
         <div>
           <el-select size="mini" v-model="page.params.classify" clearable placeholder="分类" style="width:150px;">
             <el-option
-              v-for="item in options"
+              v-for="item in classify"
               :key="item.id"
               :label="item.name"
               :value="item.id">
@@ -15,35 +15,69 @@
         <div>
           <el-select v-model="page.params.labels" clearable placeholder="标签" size="mini" style="width:100px;">
             <el-option
-              v-for="l in labels"
-              :key="l.id"
-              :label="s.name"
-              :value="s.name">
+              v-for="(l, index) in labels"
+              :key="index"
+              :label="l"
+              :value="l">
             </el-option>
           </el-select>
         </div>
-        <div>    
+        <div>
           <div>
             <el-button type="primary" size="mini" icon="el-icon-search" @click="searchNovel"></el-button>
             <el-tooltip class="item" effect="dark" content="创建小说" placement="top-start">
-              <el-button size="mini" round icon="el-icon-edit" type="success" @click="newDiary"></el-button>
+              <el-button size="mini" round icon="el-icon-edit" type="success" @click="newNovel"></el-button>
             </el-tooltip>
           </div>
         </div>
         <div class="novel_content">
           <div class="novel_list">
-            <diary-list v-for="d in page.list" :key="d.id" :diary="d" >
-              <el-button type="text" size="mini" @click="editNovel(d)">编辑</el-button>
-              <span style="color: #E4E7ED;">|</span>
-              <el-button type="text" size="mini" style="color:red" @click="deleteNovel(d)">删除</el-button>
-            </diary-list>
+            <el-table
+              ref="novelList"
+              :data="page.list"
+              tooltip-effect="dark"
+              style="width: 99%"
+              highlight-current-row
+              @selection-change="handleSelectionChange">
+              <el-table-column
+                label="书名/最新更新章节"
+                show-overflow-tooltip
+                width="400">
+                <template slot-scope="scope">
+                  <span style="color:#a6a6a6; font-weight: bold; font-size: 14px;">『{{scope.row.classifyName}}』</span>
+                  <span style="font-weight: bold; font-size: 14px;">{{scope.row.title}}</span>
+                  <span style="font-weight: 400;font-size: 12px;color: #3f5895;">{{scope.row.lastetSection === null ? '' : scope.row.lastetSection.title}}</span>
+                </template>
+              </el-table-column>
+              <el-table-column
+                label="标签"
+                width="290">
+                <template slot-scope="scope">
+                  <span style="margin-left: 2px;" v-for="(l, index) in scope.row.labelList" :key="index">{{l.name}}</span>
+                </template>
+              </el-table-column>
+              <el-table-column
+                prop="wordsNumStr"
+                label="字数"
+                width="100"
+                show-overflow-tooltip>
+              </el-table-column>
+              <el-table-column
+                width="90"
+                prop="createNikeName"
+                label="作者">
+              </el-table-column>
+              <el-table-column
+                width="120"
+                prop="stateName"
+                label="状态">
+              </el-table-column>
+            </el-table>
             <el-pagination
               v-if="page.total > 1"
               style="background: rgb(236, 245, 255);float: right;"
               :background="true"
               layout="prev, pager, next"
-              @size-change="sizeChange"
-              @current-change="currentChange"
               :current-page.sync="page.pageNum"
               :page-size="page.pageSize"
               :total="page.total">
@@ -52,6 +86,7 @@
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -63,16 +98,43 @@ import http from '@/http.js'
 export default {
   data () {
     return {
-      page: null
+      page: null,
+      novel: null, // 当前选择的小说
+      classify: null, // 分类列表
+      labels: null // 标签列表
     }
   },
   created () {
-  
+    let v = this
+    // 获取分类列表
+    http.$axiosGet(classifyUrl + '/list.re', {childType: 4}).then(res => {
+      v.classify = res !== null && res.length > 0 ? res : []
+    }).catch(err => { console.log(err) })
+
+    // 获取常用标签
+    http.$axiosGet(labelUrl + '/frequently-used.re', {type: 4}).then(res => {
+      v.labels = res !== null && res.length > 0 ? res : []
+    }).catch(err => { console.log(err) })
+
+    // 获取小说列表
+    v.page = new MyPage(10, {url: novelUrl + '/list.re'})
   },
   computed: {
 
   },
   methods: {
+    /**
+     * 表格选择行时出发方法
+     */
+    handleSelectionChange (selection) {
+      console.log(selection)
+    },
+    searchNovel () {
+
+    },
+    newNovel () {
+      this.$router.push({name: 'novelEdit'})
+    },
     editNovel (n) {
       this.$router.push({name: 'novelEdit', params: {novelOrder: n.id}})
     },
